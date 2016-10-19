3558307dee135e8f44885b401921d7acc2fe855b
stu <stu@qq.com>
2016-10-19 12:12:39 +0800

    列表

--- WebApplication4/Controllers/HomeController.cs
+++ WebApplication4/Controllers/HomeController.cs
@@ -20,6 +20,14 @@ namespace WebApplication4.Controllers
             return View();
         }
 
+        public ActionResult List()
+        {
+            string[] data = new string[] { "台风席卷海南 风力达到17级",
+            "台风逼近广西 学校停课" };
+            ViewBag.data = data;
+            return View();
+        }
+
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
